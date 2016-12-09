b6a8bb67efe2373c50b0693e5a52a39f9d2b18d3
a839606145 <alvzhixin1988@126.com>
2016-12-09 13:07:06 +0800

    delete

--- src/repast/RepastService.java
+++
@@ -1,9 +0,0 @@
-package repast;
-
-import java.util.concurrent.Semaphore;
-
-public class RepastService {
-	 volatile private Semaphore setSemaphore=new Semaphore(10);
-	 volatile private Semaphore getSemaphore=new Semaphore(8);
-
-}
